fd420c37104f0a5195f18fbe0e7ac1cae6a41d0f
IrDIE <110756720+IrDIE@users.noreply.github.com>
2024-05-11 14:23:00 +0300

    fix compilation. add feature for predictor - dynamic input shape

--- inference_sahi.py
+++ inference_sahi.py
@@ -1,10 +1,8 @@
 from utils import *
-def run_sahi_prediction(args, pt_model, source, imgsz , device):
+def run_sahi_prediction(args, pt_model, source, imgsz ):
     predictor = compile_predictor(args, pt_modelpath=pt_model, save_dir = Path('./sahi/prediction/res_SAHI/'), sahi=True, imgsz = imgsz)
-    sahi_model = get_sahi_model(pt_model,device=device)
-
     LOGGER.info('Starting SAHI prediction')
-    result = predictor(sahi_model = sahi_model, source = source)
+    result = predictor(source = source)
     return result
 
 
--- validation_sahi.py
+++ validation_sahi.py
@@ -1,19 +1,18 @@
 from utils import *
 
 
-def run_basic_validation(pt_model, yaml_datapath, args):
-    validator = compile_validator(args=args, pt_modelpath=pt_model, yaml_datapath=yaml_datapath,
+def run_basic_validation(pt_model, yaml_datapath, args, imgsz):
+    validator = compile_validator(args=args, pt_modelpath=pt_model, imgsz=imgsz, yaml_datapath=yaml_datapath,
                                   save_dir=Path('./sahi/validation/res_no_sahi/'))
     LOGGER.info('Starting basic validation')
     val_res = validator()
     return val_res
 
-def run_sahi_validation(pt_model, yaml_datapath, args, imgsz, device):
+def run_sahi_validation(pt_model, yaml_datapath, args, imgsz):
     validator_sahi = compile_validator(args=args, pt_modelpath=pt_model, yaml_datapath=yaml_datapath,
                                        save_dir=Path('./sahi/validation/res_SAHI/'), imgsz=imgsz, sahi=True)
-    sahi_model = get_sahi_model(pt_model, device = device)
     LOGGER.info('Starting SAHI validation')
-    val_res = validator_sahi(sahi_model=sahi_model)
+    val_res = validator_sahi()
     return val_res
 
 
